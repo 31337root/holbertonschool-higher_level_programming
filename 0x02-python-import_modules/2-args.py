a9c70124c93be59668e241e673fc8b2ff983d1bd
31337root <omarma@outlook.es>
2019-05-13 06:19:35 +0000

    add(Adding requierement, managing plural and singular message)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,12 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv)
 
-    if length > 1:
+    if length > 2:
         print('{:d} arguments:'.format(length - 1))
         for i in range((length - 1)):
             print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))
+    elif length == 2:
+        print('1 argument:')
+        print('1: {:s}'.format(sys.argv[1]))
     else:
         print('0 arguments.')
