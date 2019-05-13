b84c31e7ee2dd8490de753886661a3c93072f1fc
31337root <omarma@outlook.es>
2019-05-13 06:14:19 +0000

    fix(Adding requierements, first message)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@
 if __name__ == "__main__":
 
     import sys
+    length = len(sys.argv)
 
-    if len(sys.argv) > 1:
-        for i in range((len(sys.argv) - 1)):
+    if length > 1:
+        print('{:d} arguments:'.format(length - 1))
+        for i in range((length - 1)):
             print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))
     else:
         print('0 arguments.')
