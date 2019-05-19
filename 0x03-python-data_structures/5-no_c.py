2cfe308a5019dc62bbd5d02897d316216fd9143f
31337root <omarma@outlook.es>
2019-05-19 16:37:52 +0000

    Adding task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+
+    if my_string:
+        my_string = list(my_string)
+        for i in my_string:
+            if (i == 'c'):
+                my_string.remove('c')
+            if (i == 'C'):
+                my_string.remove('C')
+        my_string = "".join(my_string)
+        return my_string
+    else:
+        pass
