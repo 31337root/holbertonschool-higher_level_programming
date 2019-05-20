794fc342ace53bd12b326651a8f336079629b764
31337root <omarma@outlook.es>
2019-05-20 04:33:52 +0000

    Fixing Check conditions (Equal)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,7 +7,7 @@ def max_integer(my_list=[]):
         i = 0
         while i < len(my_list):
             if i + 1 < len(my_list):
-                if my_list[i] > my_list[i + 1]:
+                if my_list[i] >= my_list[i + 1]:
                     temp = my_list[i]
                     my_list[i + 1] = temp
             elif my_list[i] > my_list[i - 1]:
