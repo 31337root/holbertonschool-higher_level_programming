736578e57426fd9b11f8473d1e7004624d9e0701
31337root <omarma@outlook.es>
2019-05-20 05:48:54 +0000

    Adding taks 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    is_multiple = []
+    i = 0
+    for i in my_list:
+        if (i / 2) == 0:
+            is_multiple.append("True")
+        else:
+            is_multiple.append("False")
+    return is_multiple
