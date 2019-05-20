c192ca39dbd627696e3533f02116a6aad5816876
31337root <omarma@outlook.es>
2019-05-20 04:16:05 +0000

    Adding task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if my_list:
+        i = 0
+        while i < len(my_list):
+            if i + 1 < len(my_list):
+                if my_list[i] > my_list[i + 1]:
+                    temp = my_list[i]
+                    my_list[i + 1] = temp
+            i += 1
+        return temp
+    else:
+        return None
