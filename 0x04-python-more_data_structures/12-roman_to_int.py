72c7416925bd7027a2c2cc0cfcaba0e8e59b18e0
31337root <omarma@outlook.es>
2019-05-26 00:30:37 +0000

    Fixing principal condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 def roman_to_int(roman_string):
 
-    if type(roman_string) is str or roman_string is not None:
+    if type(roman_string) is str and roman_string is not None:
 
         roman_numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                          'D': 500, 'M': 1000}
