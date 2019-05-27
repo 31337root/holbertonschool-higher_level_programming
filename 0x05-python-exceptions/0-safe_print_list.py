f3073cf0cd3180521a297e65fe6202274ba6f1b2
31337root <omarma@outlook.es>
2019-05-27 22:14:23 +0000

    Adding taskl 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    try:
+        i = 0
+
+        for k in range(x):
+            print(my_list[k], end="")
+            i += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return i
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,2 @@
+# 0x05. Python - Exceptions
+# 0-safe_print_list.py - function that prints x elements of a list.
