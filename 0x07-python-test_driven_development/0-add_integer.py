ba228d8d1bea653de60311f02f29ba060233baef
31337root <omarma@outlook.es>
2019-06-05 23:22:10 +0000

    Fixing PEP8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,7 +6,7 @@ def add_integer(a, b=98):
     """Function to add two integers."""
 
     if ((type(a) is int or type(a) is float) and
-    (type(b) is int or type(b) is float)):
+       (type(b) is int or type(b) is float)):
         return int(a) + int(b)
     elif a is None or (type(a) is not int and type(a) is not float):
         raise TypeError('a must be an integer')
