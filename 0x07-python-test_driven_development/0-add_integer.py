e5537ee98792080570165348033d18deec07f55f
31337root <omarma@outlook.es>
2019-06-05 23:10:20 +0000

    Adding task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+
+    """Function to add two integers."""
+
+    if (type(a) is int or type(a) is float) and (type(b) is int or type(b) is float):
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        return a + b
+    elif type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
