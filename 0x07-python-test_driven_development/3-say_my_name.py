e7f93462ac32079d15cc3468c6aa2dc529286f2b
31337root <omarma@outlook.es>
2019-06-06 01:51:47 +0000

    Adding missing arguments edge case

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,10 +3,12 @@
 
 def say_my_name(first_name, last_name=""):
 
-    if type(first_name) is str:
-        if type(last_name) is str:
-            print("My name is {:s} {:s}".format(first_name, last_name))
+    if first_name:
+        if type(first_name) is str:
+            if type(last_name) is str:
+                print("My name is {:s} {:s}".format(first_name, last_name))
+            else:
+                raise TypeError("last_name must be a string")
         else:
-            raise TypeError("last_name must be a string")
-    else:
-        raise TypeError("first_name must be a string")
+            raise TypeError("first_name must be a string")
+    pass
