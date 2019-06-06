08906e876d9c97109fa82d132f0ad6150a1f209c
31337root <omarma@outlook.es>
2019-06-06 03:35:41 +0000

    Adding task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def text_indentation(text):
+
+    i = 0
+
+    if text:
+        if type(text) is str:
+            for char in text:
+                if char is '.' or char is '?' or char is ':':
+                    print(char)
+                    print()
+                    i = 1
+                    continue
+                if i == 1:
+                    i = 0
+                    continue
+                print(char, end="")
+        else:
+            raise TypeError("text must be a string")
+    else:
+        pass
