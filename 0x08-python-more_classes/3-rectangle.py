5f1ad76d5fd488fd8dc8953dc552071195c407d1
31337root <omarma@outlook.es>
2019-06-08 00:44:38 +0000

    Adding task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+    """Store the wigth and height of a Rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        rectangle_str = ""
+
+        for h in range(self.__height):
+            for w in range(self.__width):
+                rectangle_str += '#'
+            if h < (self.__height - 1):
+                rectangle_str += '\n'
+        return rectangle_str
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is int:
+            if value > -1:
+                self.__width = value
+            else:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @height.setter
+    def height(self, value):
+        if type(value) is int:
+            if value > -1:
+                self.__height = value
+            else:
+                raise ValueError("height must be >= 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+
+        """Find the rectangle area."""
+
+        return self.__height * self.__width
+
+    def perimeter(self):
+
+        """Find the rectangle perimeter."""
+
+        if self.__height is not 0 and self.__width is not 0:
+            return (self.__height * 2) + (self.__width * 2)
+        else:
+            return 0
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -2,3 +2,4 @@
 # 0-rectangle.py - Empty class Rectangle that defines a rectangle.
 # 1-rectangle.py - Class Rectangle that defines a rectangle by width and height.
 # 2-rectangle.py - Same but new methods area() and perimeter().
+# 3-rectangle.py - Same but new __str__ method defined.
