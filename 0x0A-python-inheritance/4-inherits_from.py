67e9038f3178ea73c546e5074c42a0eff3953ca0
31337root <omarma@outlook.es>
2019-06-10 18:33:54 +0000

    Fixing PEP8 coding style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 def inherits_from(obj, a_class):
 
     """Function that returns True if the object is an instance
-    of a class that inherited (directly or indirectly) from the 
+    of a class that inherited (directly or indirectly) from the
     specified class ; otherwise False."""
 
     if a_class is not None:
