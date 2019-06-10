58819d3b8d4961117c8dec4dd3faf17365aa0a8f
31337root <omarma@outlook.es>
2019-06-10 20:46:43 +0000

    Deleting dafults parameter

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name=None, value=None):
+    def integer_validator(self, name, value):
         if name is not None and value is not None:
             if type(value) is not int:
                 raise TypeError(name + " must be an integer")
