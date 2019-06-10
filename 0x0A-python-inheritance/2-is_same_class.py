dfe8e23730da21854a138f0143db89f4378dc2b3
31337root <omarma@outlook.es>
2019-06-10 03:58:41 +0000

    Adding None edge case

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ def is_same_class(obj, a_class):
 
     """Returns True if the object is exactly an instance
     of the specified class ; otherwise False."""
-    if obj and a_class:
+    if obj is not None and a_class is not None:
         if type(obj) is a_class:
             return True
         else:
