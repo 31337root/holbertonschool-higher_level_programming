3c9cb4734d1d4d628af26fdda7838563df66ff47
31337root <omarma@outlook.es>
2019-06-10 20:38:20 +0000

    Fixing PEP8 coding style

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name = None, value = None):
+    def integer_validator(self, name=None, value=None):
         if name is not None and value is not None:
             if type(value) is not int:
                 raise TypeError(name + " must be an integer")
