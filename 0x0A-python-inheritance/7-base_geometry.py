ab926f1b4665b099cd4c4a1af6987d7862ddda51
31337root <omarma@outlook.es>
2019-06-10 20:37:16 +0000

    Fixing entry conditional

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,9 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name = None, value = None):
+        if name is not None and value is not None:
             if type(value) is not int:
                 raise TypeError(name + " must be an integer")
             if value < 1:
                 raise ValueError(name + " must be greater than 0")
+        return None
