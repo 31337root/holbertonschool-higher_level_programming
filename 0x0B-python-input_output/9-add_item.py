85f689eb8739e571cedbc72504f4853d180dcded
31337root <omarma@outlook.es>
2019-06-13 02:29:38 +0000

    Adding module documentation

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+"""Script that adds all arguments to a Python list,
+and then save them to a file."""
+
 import sys
 import json
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-
-"""Script that adds all arguments to a Python list, and then save them to a file."""
 
 with open("add_item.json", 'a') as temp:
     a_list = []
