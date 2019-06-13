de31b6ba49b3d2be519f60877a5e6594750a0324
31337root <omarma@outlook.es>
2019-06-13 02:42:31 +0000

    Adding task 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,13 +4,14 @@ import sys
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-with open("add_item.json", 'a') as temp:
+a_list = []
 
-    a_list = []
+try:
     a_list = load_from_json_file("add_item.json")
 
-    for i in range(len(sys.argv)):
-        if i > 0:
-            a_list.append(sys.argv[i])
+    for i in argv[1:]:
+        a_list.append(i)
+except:
+    pass
 
     save_to_json_file(a_list, "add_item.json")
